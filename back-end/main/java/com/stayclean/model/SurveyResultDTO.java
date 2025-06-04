333bb10723f8025d8f0267d94d5cbe43982f36bb
huuanh1411 <anhhuu312@gmail.com>
2025-06-05 01:19:03 +0700

    add SurveyResultDTO class with properties and methods

---
+++ back-end/main/java/com/stayclean/model/SurveyResultDTO.java
@@ -0,0 +1,74 @@
+package com.stayclean.model;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+public class SurveyResultDTO {
+    private int surveyResultID;
+    private UserDTO user;
+    private SurveyDTO survey;
+    private Map<QuestionDTO, QuestionDTO.answer> answers;
+    private String recommendation;
+    private LocalDateTime surveyTime;
+public SurveyResultDTO() {
+        // Default constructor
+    }
+    public SurveyResultDTO(int surveyResultID, UserDTO user, SurveyDTO survey, Map<QuestionDTO, QuestionDTO.answer> answers,
+                           String recommendation, LocalDateTime surveyTime) {
+        this.surveyResultID = surveyResultID;
+        this.user = user;
+        this.survey = survey;
+        this.answers = answers;
+        this.recommendation = recommendation;
+        this.surveyTime = surveyTime;
+    }
+    public int getSurveyResultID() {
+        return surveyResultID;
+    }
+
+    public void setSurveyResultID(int surveyResultID) {
+        this.surveyResultID = surveyResultID;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public SurveyDTO getSurvey() {
+        return survey;
+    }
+
+    public void setSurvey(SurveyDTO survey) {
+        this.survey = survey;
+    }
+
+    public Map<QuestionDTO, QuestionDTO.answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Map<QuestionDTO, QuestionDTO.answer> answers) {
+        this.answers = answers;
+    }
+
+    public String getRecommendation() {
+        return recommendation;
+    }
+
+    public void setRecommendation(String recommendation) {
+        this.recommendation = recommendation;
+    }
+
+    public LocalDateTime getSurveyTime() {
+        return surveyTime;
+    }
+
+    public void setSurveyTime(LocalDateTime surveyTime) {
+        this.surveyTime = surveyTime;
+    }
+
+}
