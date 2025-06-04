7b06fb0af56eb95c80b6ba23afe59eb10f0b68b9
huuanh1411 <anhhuu312@gmail.com>
2025-06-05 01:21:37 +0700

    add ConsulationDTO, ConsultantDTO, and ConsultantNoteDTO

---
+++ back-end/main/java/com/stayclean/model/ConsultantDTO.java
@@ -0,0 +1,10 @@
+package com.stayclean.model;
+public class ConsultantDTO extends UserDTO {
+public ConsultantDTO() {
+    super();
+}
+    public void viewSchedule() {
+    // Logic to view schedule
+}
+
+}
