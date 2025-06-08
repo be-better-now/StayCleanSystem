3499dc3b396fd9410e0c0162f8bd7d29e9df9387
huuanh1411 <anhhuu312@gmail.com>
2025-06-08 16:56:17 +0700

    add springboot and JPA

---
+++ back-end/test/java/com/stayclean/StayCleanSystem/StayCleanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayclean.StayCleanSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayCleanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
