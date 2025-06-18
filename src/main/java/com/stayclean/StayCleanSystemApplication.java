9d42afdcded3a80b858c1239e1d4e4c7c7fb1bed
be-better-now <huuduy.uwu@gmail.com>
2025-06-18 22:36:31 +0700

    add OpenAPI definition to Application class

--- src/main/java/com/stayclean/StayCleanSystemApplication.java
+++ src/main/java/com/stayclean/StayCleanSystemApplication.java
@@ -1,9 +1,18 @@
 package com.stayclean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "StayClean API",
+                version = "1.0",
+                description = "Backend API for StayClean web application"
+        )
+)
 public class StayCleanSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(StayCleanSystemApplication.class, args);
