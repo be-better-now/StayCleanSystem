11359ae088b147a7eed826c57ce4a3d6eaeb9905
be-better-now <huuduy.uwu@gmail.com>
2025-06-18 22:51:19 +0700

    add JWT bearer token security scheme (@SecurityChema) to Swagger

--- src/main/java/com/stayclean/StayCleanSystemApplication.java
+++ src/main/java/com/stayclean/StayCleanSystemApplication.java
@@ -1,7 +1,10 @@
 package com.stayclean;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 version = "1.0",
                 description = "Backend API for StayClean web application"
         )
+)
+
+@SecurityScheme(
+        name = "api", // Bắt buộc trùng với name sử dụng trong @SecurityRequirement
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class StayCleanSystemApplication {
     public static void main(String[] args) {
