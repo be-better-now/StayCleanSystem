4ec8e337f1575b2925e097aed7c2001fe4687bb9
be-better-now <huuduy.uwu@gmail.com>
2025-06-27 13:45:43 +0700

    update CRUD CourseService - complete!!!

--- src/main/java/com/stayclean/repository/CourseRepository.java
+++ src/main/java/com/stayclean/repository/CourseRepository.java
@@ -4,6 +4,9 @@ import com.stayclean.entity.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<CourseEntity, String> {
+    List<CourseEntity> findByCourseNameContainingIgnoreCase(String keyword);
 }
--- src/main/java/com/stayclean/services/CourseService.java
+++ src/main/java/com/stayclean/services/CourseService.java
@@ -20,17 +20,29 @@ public class CourseService {
         return courseRepository.save(courseEntity);
     }
 
+    public CourseEntity getCourseById(String courseId) {
+        return courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Course not found with id: " + courseId));
+    }
+
+
+    public List<CourseEntity> searchCoursesByName(String keyword) {
+        return courseRepository.findByCourseNameContainingIgnoreCase(keyword);
+    }
+
     public CourseEntity updateCourse(String courseId, CourseEntity updatedCourse) {
         return courseRepository.findById(courseId).map(course -> {
-                    course.setCourseName(updatedCourse.getCourseName());
-                    course.setCourseDescription(updatedCourse.getCourseDescription());
-                    course.setCourseDate(updatedCourse.getCourseDate());
-                    course.setCourseCategory(updatedCourse.getCourseCategory());
-                    course.setAgeGroup(updatedCourse.getAgeGroup());
-                    // Add other fields as needed
-                    return courseRepository.save(course);
-                })
-                .orElseThrow(() -> new RuntimeException("Course not found with id: " + courseId));
+            course.setCourseName(updatedCourse.getCourseName());
+            course.setCourseDescription(updatedCourse.getCourseDescription());
+            course.setCourseDate(updatedCourse.getCourseDate());
+            course.setCourseCategory(updatedCourse.getCourseCategory());
+            course.setAgeGroup(updatedCourse.getAgeGroup());
+            course.setCourseStatus(updatedCourse.getCourseStatus());
+            course.setProgram(updatedCourse.getProgram());
+            course.setCourseImg(updatedCourse.getCourseImg());
+
+            return courseRepository.save(course);
+        }).orElseThrow(() -> new RuntimeException("Course not found with id: " + courseId));
     }
     public void deleteCourse(String courseId) {
         courseRepository.deleteById(courseId);
